83749be3d6a8daa4b7de606fd6f1dbaf954bd5a5
David Knapp <dknapp@blueboltsolutions.com>
2021-04-21 14:33:32 -0500

    adding app provider wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,39 @@
 import { Layout, Page, Button, Card } from '@shopify/polaris';
 import { useState } from 'react';
+import translations from '@shopify/polaris/locales/en.json';
 
 const Index = () => {
-    const [loading, setLoading] = useState(false);
-    const handleButton = () => {
-        setLoading(true);
-        const res = fetch(`/reindex`, {
-            method: 'GET'
-        }).then(data => { 
-            setLoading(false);
-        });
-    };
-    return (
-        <Page>
-            <Layout>
-                <Layout.Section>
-                    <Card title="Author JSON Updater" sectioned> 
-                        <p>You should re-index your Author JSON after changing author blogs.</p>
-                    </Card>
-                </Layout.Section>
-                <Layout.Section>
-                    <Button
-                        loading={loading}
-                        primary={true}
-                        size={"large"}
-                        onClick={handleButton}
-                    >Re-index</Button>
-                </Layout.Section>
-                
-            </Layout>
-        </Page>
-    )
+  const [loading, setLoading] = useState(false);
+  const handleButton = () => {
+    setLoading(true);
+    const res = fetch(`/reindex`, {
+      method: 'GET'
+    }).then(data => {
+      setLoading(false);
+    });
+  };
+  return (
+    <AppProvider i18n={translations}>
+      <Page>
+        <Layout>
+          <Layout.Section>
+            <Card title="Author JSON Updater" sectioned>
+              <p>You should re-index your Author JSON after changing author blogs.</p>
+            </Card>
+          </Layout.Section>
+          <Layout.Section>
+            <Button
+              loading={loading}
+              primary={true}
+              size={"large"}
+              onClick={handleButton}
+            >Re-index</Button>
+          </Layout.Section>
+
+        </Layout>
+      </Page>
+    </AppProvider>
+  )
 };
-  
-  export default Index;
+
+export default Index;
