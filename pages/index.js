a19fa75d0b4232f8ba6a3c22ec31d4ea2525377b
David Knapp <dknapp@blueboltsolutions.com>
2021-04-23 15:20:16 -0500

    Updated version of the app, can perform action no queueing

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,39 @@
-import { AppProvider, Layout, Page, Button, Card } from '@shopify/polaris';
+import { Layout, Page, Button, Card } from '@shopify/polaris';
 import { useState } from 'react';
-import translations from '@shopify/polaris/locales/en.json';
+import store from 'store-js';
+import Cookies from 'js-cookie';
+
 
 const Index = () => {
-  const [loading, setLoading] = useState(false);
-  const handleButton = () => {
-    setLoading(true);
-    const res = fetch(`/reindex`, {
-      method: 'GET'
-    }).then(data => {
-      setLoading(false);
-    });
-  };
-  return (
-    <AppProvider i18n={translations}>
-      <Page>
-        <Layout>
-          <Layout.Section>
-            <Card title="Author JSON Updater" sectioned>
-              <p>You should re-index your Author JSON after changing author blogs.</p>
-            </Card>
-          </Layout.Section>
-          <Layout.Section>
-            <Button
-              loading={loading}
-              primary={true}
-              size={"large"}
-              onClick={handleButton}
-            >Re-index</Button>
-          </Layout.Section>
-
-        </Layout>
-      </Page>
-    </AppProvider>
-  )
+    const [loading, setLoading] = useState(false);
+    const handleButton = () => {
+        setLoading(true);
+        const res = fetch(`/reindex`, {
+            method: 'GET'
+        }).then(data => { 
+            setLoading(false);
+        });
+    };
+    return (
+        <Page>
+            <Layout>
+                <Layout.Section>
+                    <Card title="Author JSON Updater" sectioned>
+                        <p>You should re-index your Author JSON after changing author blogs.</p>
+                    </Card>
+                </Layout.Section>
+                <Layout.Section>
+                    <Button
+                        loading={loading}
+                        primary={true}
+                        size={"large"}
+                        onClick={handleButton}
+                    >Re-index</Button>
+                </Layout.Section>
+                
+            </Layout>
+        </Page>
+    )
 };
 
 export default Index;
--- server.js
+++ server.js
@@ -1,74 +1,144 @@
 require('isomorphic-fetch');
 const dotenv = require('dotenv');
+dotenv.config();
 const Koa = require('koa');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
-const { default: Shopify, ApiVersion } = require('@shopify/shopify-api');
+const session = require('koa-session');
+const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 const Router = require('koa-router');
-
-dotenv.config();
-
-Shopify.Context.initialize({
-  API_KEY: process.env.SHOPIFY_API_KEY,
-  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  SCOPES: process.env.SHOPIFY_API_SCOPES.split(","),
-  HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
-  API_VERSION: ApiVersion.October20,
-  IS_EMBEDDED_APP: true,
-  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
-});
+const cheerio = require('cheerio');
+const { version } = require('cheerio');
 
 const port = parseInt(process.env.PORT, 10) || 3005;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const ACTIVE_SHOPIFY_SHOPS = {};
+const {
+  SHOPIFY_API_SECRET_KEY,
+  SHOPIFY_API_KEY,
+  HOST,
+} = process.env;
 
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
-  server.keys = [Shopify.Context.API_SECRET_KEY];
+  server.use(session({ sameSite: 'none', secure: true }, server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
 
   server.use(
     createShopifyAuth({
-      afterAuth(ctx) {
-        const { shop, scope } = ctx.state.shopify;
-        ACTIVE_SHOPIFY_SHOPS[shop] = scope;
-
-        ctx.redirect(`/?shop=${shop}`);
-      },
-    }),
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ['read_products', 'write_products', 'read_themes', 'write_themes'],
+      async afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+        ctx.cookies.set("shopOrigin", shop, {
+          httpOnly: false,
+          secure: true,
+          sameSite: 'none'
+        });
+      }
+    })
   );
 
-  const handleRequest = async (ctx) => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-    ctx.res.statusCode = 200;
-  };
+  server.use(graphQLProxy({ version: ApiVersion.July20 }));
 
-  router.get("/", async (ctx) => {
-    const shop = ctx.query.shop;
+  router.get('/reindex', verifyRequest(), async (ctx) => {
+    try {
+      //let job = await workQueue.add({ accessToken: ctx.session.accessToken, shop: ctx.session.shop });
+      //console.log('job.id', job.id);
+      var accessToken = ctx.session.accessToken;
+      var shop = ctx.session.shop;
+      var page = 1;
+      responseData = [];
+      allResponseData = [];
+      do {
+        var response = await fetch(`https://${shop}/blogs/authors?page=${page}&view=authors-json`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            "X-Shopify-Access-Token": accessToken,
+          }
+        });
+        var responseText = await response.text();
+        var $ = cheerio.load(responseText);
+        var jsonText = $('body').html();
+        jsonText = jsonText.replace(/&quot;/g, '"');
+        responseData = JSON.parse(jsonText);
+        allResponseData = allResponseData.concat(responseData);
+        console.log('Download 1000 authors. Page:', page);
+        //job.progress();
+        page++;
+      } while (responseData.length > 0)
+      console.log('Downloaded all authors, total:', allResponseData.length);
+        var authorNavItems = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,num'.split(',');
+        authorData = {};
+        authorNavItems.forEach(item => {
+            authorData[item] = [];
+        });
+        allResponseData.forEach(d => {
+            var array = authorData[d.name[0].toUpperCase()];
+            if (array == undefined) {
+                authorData['num'].push(d);
+            } else {
+                array.push(d);
+            }
+        });
+        authorNavItems.forEach(item => {
+            authorData[item] = authorData[item].sort(function (a, b) {
+                a = a.name.toLowerCase();
+                b = b.name.toLowerCase();
+                if (a > b) return 1;
+                if (a < b) return -1;
+                return 0;
+            });
+        });
 
-    if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
-      ctx.redirect(`/auth?shop=${shop}`);
-    } else {
-      await handleRequest(ctx);
+        authorNavItems.forEach(async item => {
+          try {
+            let url = `https://${shop}/admin/api/2020-04/themes/${process.env.THEME_ID}/assets.json`;
+            var response = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "X-Shopify-Access-Token": accessToken,
+                },
+                body: `{
+                "asset": {
+                    "key": "templates/blog.author-json-${item}.liquid",
+                    "value": "{% layout none %} ${JSON.stringify(authorData[item]).replace(/"/g, '\\"')}"
+                }
+            }`
+            });
+            if(response.ok) {
+              console.log('Uploading JSON to Shopify:', item, authorData[item].length);
+            } else {
+              console.error('Failed Uploading JSON to Shopify:', item, response.status, response.statusText);
+            }
+          } catch (err) {
+            console.error(err.message);
+            console.error('Failed Uploading JSON to Shopify:', item, authorData[item].length);
+          }
+
+        });
+      ctx.respond = true;
+      ctx.res.statusCode = 200;
+    } catch (err) {
+      ctx.respond = true;
+      ctx.res.statusMessage = err.message;
+      ctx.res.statusCode = 200;
     }
   });
 
-  router.get('/reindex', verifyRequest(), async (ctx) => {
-      debugger
-      //let job = await workQueue.add({ accessToken: ctx.session.accessToken, shop: ctx.session.shop });
-      //console.log('job.id', job.id);
-      ctx.respond = true;
-      ctx.res.statusCode = 200;
+  router.get('(.*)', verifyRequest(), async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
   });
-
-  router.get("(/_next/static/.*)", handleRequest);
-  router.get("/_next/webpack-hmr", handleRequest);
-  router.get("(.*)", verifyRequest(), handleRequest);
 
   server.use(router.allowedMethods());
   server.use(router.routes());
